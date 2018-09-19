3e5bc09cc40e7bea2ab31f7deb2c5143f82fa0d5
Juan <gomezcabo@gmail.com>
2018-09-19 09:12:16 +0100

    Cytoscape integration

---
+++ src/components/Graph/GraphEdge.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  props: ['cy', 'edge'],
+
+  mounted() {
+    this.cy.add({
+      group: 'edges',
+      id: this.edge.id,
+      data: { ...this.edge },
+      classes: this.edge.label ? 'label' : null
+    })
+  },
+
+  render() {
+    return null
+  }
+}
+</script>
---
+++ src/components/Graph/GraphNode.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  props: ['cy', 'node'],
+
+  mounted() {
+    this.cy.add({
+      group: 'nodes',
+      id: this.node.id,
+      data: { ...this.node }
+    })
+  },
+
+  render() {
+    return null
+  }
+}
+</script>
